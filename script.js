a0a66c2207f3f15b1a9cb358a71316cd3da841a2
Manuela França <manuelaalves26@gmail.com>
2022-05-01 15:24:25 -0300

    Indentação e atribuição de uma variável.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const colorSelect = document.querySelectorAll('.color');
 const pixelBoard = document.querySelector('#pixel-board');
 
-//Adicionou e removeu a classe selected - Requisito 7
+// Adicionou e removeu a classe selected - Requisito 7
 function addSelected(event) {
   const selectedRemove = document.querySelector('.selected');
   selectedRemove.classList.remove('selected');
@@ -10,27 +10,27 @@ function addSelected(event) {
 for (const color of colorSelect) {
   color.addEventListener('click', addSelected);
 }
-//Preenche os quadros com a cor selecionada - Requisito 8
+// Preenche os quadros com a cor selecionada - Requisito 8
 function selectColor(event) {
   const color = document.querySelector('.selected');
   const background = window.getComputedStyle(color).getPropertyValue('background-color');
   event.target.style.background = background;
 }
-//Preenche os quadros com a cor branca - Requisito 9
+// Preenche os quadros com a cor branca - Requisito 9
 function clearPixel() {
   const clearButton = document.getElementById('clear-board');
   const square = document.querySelectorAll('.pixel');
   const clearColor = 'white';
 
-  clearButton.addEventListener('click', function() {
-    for (index = 0; index < square.length; index += 1) {
+  clearButton.addEventListener('click', function () {
+    for (let index = 0; index < square.length; index += 1) {
       if (square[index].style.backgroundColor !== 'white') {
         square[index].style.backgroundColor = clearColor;
       }
     }
-  })
+  });
 }clearPixel();
-//Criou o quadro de Pixels - Requisito 4
+// Criou o quadro de Pixels - Requisito 4
 function createPixelsBoard(size) {
   for (let index = 0; index < size * size; index += 1) {
     const createDiv = document.createElement('div');
